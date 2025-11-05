e0476b792c1ebdbe38db5f560bff7b787414bbcb
petrowic-n <nemanjapetrovic358@gmail.com>
2025-11-05 17:15:09 +0100

    classes section finished

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/classes.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
