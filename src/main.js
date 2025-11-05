4c77f4cb31040be40269784f735e1cbd6b0bc3c7
savanpecic <savanpecic@gmail.com>
2025-11-05 16:50:56 +0100

    odrađene selekcije

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/why.css";
+import "../styles/components/pricing.css";
+import "../styles/components/testimonial.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
